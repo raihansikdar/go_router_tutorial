6513f967110815e073ae8fcfaf64bee40ed794c6
raihan sikdar <raihansikdar10@gmail.com>
2024-12-25 23:36:41 +0600

    add name route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router_tutorial/app_router/app_routing.dart';
-import 'package:go_router_tutorial/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_router_tutorial/app_router/app_routing.dart';
 
 class ProfileScreen extends StatelessWidget {
   final int userId;
@@ -10,18 +9,18 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Profile"),
+      appBar: AppBar(title: const Text("Profile"),
       leading: GestureDetector(
           onTap: (){
             context.goNamed("home");
           },
-          child: Icon(Icons.arrow_back)),
+          child: const Icon(Icons.arrow_back)),
       ),
       body: Center(child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Profile Screen",style: TextStyle(fontSize: 25),),
-          Text(" id: $userId name: $userName",style: TextStyle(fontSize: 25),),
+          const Text("Profile Screen",style: TextStyle(fontSize: 25),),
+          Text(" id: $userId name: $userName",style: const TextStyle(fontSize: 25),),
         ],
       ),),
     );
